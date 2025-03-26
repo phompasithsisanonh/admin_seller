f866533ddddf7200a5af17f7129e50c2aa6f546e
phompasith sisanonh <98532227+phompasithsisanonh@users.noreply.github.com>
2025-03-26 15:04:22 +0700

    lat

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : 'http://localhost:5000/api'
+    baseURL : 'https://backendecommerc2e-production.up.railway.app/api'
 })
 
 export default api
